3163d37ac702635f4d0ec07887ee12d4d471db44
Yuttadhammo Bhikkhu <yuttadhammo@gmail.com>
2015-11-06 02:29:09 +0000

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,216 +1,261 @@
--- MySQL dump 10.13  Distrib 5.5.44, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: meditation_old
--- ------------------------------------------------------
--- Server version	5.5.44-0ubuntu0.14.04.1
+-- Host: localhost
+-- Generation Time: Nov 06, 2015 at 02:28 AM
+-- Server version: 5.5.46-0ubuntu0.14.04.2
+-- PHP Version: 5.5.9-1ubuntu4.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Database: `meditation_old`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `anumodana`
 --
 
-DROP TABLE IF EXISTS `anumodana`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `anumodana` (
+CREATE TABLE IF NOT EXISTS `anumodana` (
   `uid` int(20) NOT NULL,
   `sid` bigint(20) NOT NULL,
   UNIQUE KEY `constraint` (`uid`,`sid`),
-  KEY `Session ID` (`sid`),
-  CONSTRAINT `sessions` FOREIGN KEY (`sid`) REFERENCES `sessions` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `Session ID` (`sid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anumodana_chat`
+--
+
+CREATE TABLE IF NOT EXISTS `anumodana_chat` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UQ_CID_UID` (`cid`,`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=678 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointments`
+--
+
+CREATE TABLE IF NOT EXISTS `appointments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `aid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `aid` (`aid`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=252 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appointment_slots`
+--
+
+CREATE TABLE IF NOT EXISTS `appointment_slots` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `day` int(1) NOT NULL,
+  `time` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=49 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `chats`
 --
 
-DROP TABLE IF EXISTS `chats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `chats` (
+CREATE TABLE IF NOT EXISTS `chats` (
   `cid` bigint(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
   `time` datetime NOT NULL,
-  `message` varchar(255) NOT NULL,
+  `message` mediumtext CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`cid`),
-  KEY `fk_uid` (`uid`),
-  CONSTRAINT `fk_uid` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6585 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  KEY `fk_uid` (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11746 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `commitments`
 --
 
-DROP TABLE IF EXISTS `commitments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `commitments` (
+CREATE TABLE IF NOT EXISTS `commitments` (
   `cid` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(64) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
+  `title` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `creatorid` int(11) NOT NULL,
-  `period` varchar(10) NOT NULL,
+  `period` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `day` int(3) NOT NULL,
-  `time` varchar(5) NOT NULL,
-  `length` varchar(100) NOT NULL,
+  `time` varchar(5) COLLATE utf8_unicode_ci NOT NULL,
+  `length` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`cid`)
-) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=52 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `logins`
 --
 
-DROP TABLE IF EXISTS `logins`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `logins` (
+CREATE TABLE IF NOT EXISTS `logins` (
   `uid` bigint(11) NOT NULL DEFAULT '0',
-  `token` varchar(2550) CHARACTER SET utf8 NOT NULL,
+  `token` varchar(2550) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `uid` (`uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `misc`
 --
 
-DROP TABLE IF EXISTS `misc`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `misc` (
-  `key` varchar(255) NOT NULL,
-  `value` varchar(2550) DEFAULT '',
+CREATE TABLE IF NOT EXISTS `misc` (
+  `key` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `value` varchar(2550) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`key`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `quotes`
 --
 
-DROP TABLE IF EXISTS `quotes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `quotes` (
+CREATE TABLE IF NOT EXISTS `quotes` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
   `quote` varchar(8000) NOT NULL,
   `cite` varchar(255) DEFAULT NULL,
   `link` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=366 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=366 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `reset`
 --
 
-DROP TABLE IF EXISTS `reset`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `reset` (
+CREATE TABLE IF NOT EXISTS `reset` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
-  `reset_key` varchar(128) NOT NULL,
+  `reset_key` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uid` (`uid`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=19 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `schedule`
 --
 
-DROP TABLE IF EXISTS `schedule`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `schedule` (
+CREATE TABLE IF NOT EXISTS `schedule` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `time` varchar(4) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `description` varchar(2550) NOT NULL DEFAULT '',
+  `time` varchar(4) COLLATE utf8_unicode_ci NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(2550) COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `sessions`
 --
 
-DROP TABLE IF EXISTS `sessions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sessions` (
+CREATE TABLE IF NOT EXISTS `sessions` (
   `sid` bigint(11) NOT NULL AUTO_INCREMENT,
   `uid` int(10) NOT NULL,
   `start` datetime NOT NULL,
   `walking` int(3) NOT NULL,
   `sitting` int(3) NOT NULL,
   `end` datetime NOT NULL,
-  `type` varchar(10) DEFAULT NULL,
+  `type` varchar(10) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB AUTO_INCREMENT=11236 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=17927 ;
 
---
--- Table structure for table `user_commitments`
---
-
-DROP TABLE IF EXISTS `user_commitments`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user_commitments` (
-  `aid` int(11) NOT NULL AUTO_INCREMENT,
-  `cid` int(11) NOT NULL,
-  `uid` int(11) NOT NULL,
-  PRIMARY KEY (`aid`),
-  UNIQUE KEY `commit` (`cid`,`uid`),
-  CONSTRAINT `FK_commitments` FOREIGN KEY (`cid`) REFERENCES `commitments` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=549 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(20) NOT NULL,
-  `password` char(40) NOT NULL,
-  `email` varchar(50) DEFAULT '',
+  `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `password` char(40) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `show_email` int(1) NOT NULL DEFAULT '0',
-  `website` varchar(100) DEFAULT '',
-  `description` varchar(255) DEFAULT '',
-  `country` varchar(50) DEFAULT '',
-  `img` varchar(255) DEFAULT '',
+  `website` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `country` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `img` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`uid`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=474 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=704 ;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_commitments`
+--
+
+CREATE TABLE IF NOT EXISTS `user_commitments` (
+  `aid` int(11) NOT NULL AUTO_INCREMENT,
+  `cid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  PRIMARY KEY (`aid`),
+  UNIQUE KEY `commit` (`cid`,`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=751 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `anumodana`
+--
+ALTER TABLE `anumodana`
+  ADD CONSTRAINT `sessions` FOREIGN KEY (`sid`) REFERENCES `sessions` (`sid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `appointments`
+--
+ALTER TABLE `appointments`
+  ADD CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`aid`) REFERENCES `appointment_slots` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `chats`
+--
+ALTER TABLE `chats`
+  ADD CONSTRAINT `fk_uid` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_commitments`
+--
+ALTER TABLE `user_commitments`
+  ADD CONSTRAINT `FK_commitments` FOREIGN KEY (`cid`) REFERENCES `commitments` (`cid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-09-02 21:39:03
