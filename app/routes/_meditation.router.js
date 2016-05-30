d7f403f492db2ae7236bf5c52103ec0124f27e65
Martin <bieder.martin@googlemail.com>
2016-05-30 22:39:17 +0200

    fix(mediation): fixes a bug with empty values

--- app/routes/_meditation.router.js
+++ app/routes/_meditation.router.js
@@ -79,10 +79,10 @@ export default (app, router) => {
    */
   router.post('/api/meditation', (req, res) => {
     // parse input and normalize
-    let walking = parseInt(req.body.walking, 10);
+    let walking = req.body.walking ? parseInt(req.body.walking, 10) : 0;
     if (walking > 120) walking = 120;
     if (walking < 0) walking = 0;
-    let sitting = parseInt(req.body.sitting, 10);
+    let sitting = req.body.sitting ? parseInt(req.body.sitting, 10) : 0;
     if (sitting > 120) sitting = 120;
     if (sitting < 0) sitting = 0;
     let total = sitting + walking;
