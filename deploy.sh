fa61076f461ec1768baa28cff62aa737dfc6dbe7
Martin <bieder.martin@googlemail.com>
2016-07-31 13:20:32 +0200

    chore(travis): fixes bash syntax error

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ cp -R node_modules dist/node_modules
 echo "{ \"version\": \"$TRAVIS_TAG\" }" > dist/version.json
 tar -czf transfer.tgz dist
 scp -o "StrictHostKeyChecking no" transfer.tgz jenkins@159.203.18.58:/var/www/meditation-plus
-ssh -o "StrictHostKeyChecking no" jenkins@159.203.18.58 'cd /var/www/meditation-plus; cp config/config.json ./; rm -rf !(client|config.json|transfer.tgz); tar -xzf transfer.tgz  --strip 1; mv config.json config/config.json; rm transfer.tgz'
+ssh -o "StrictHostKeyChecking no" jenkins@159.203.18.58 'cd /var/www/meditation-plus; cp config/config.json ./; rm -rf app .babelrc config server.conf server.js sockets node_modules version.json; tar -xzf transfer.tgz --strip 1; mv config.json config/config.json; rm transfer.tgz'
