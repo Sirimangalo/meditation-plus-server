746a5993c6b158d07bb9ac307f7ae786a58845a4
Martin <bieder.martin@googlemail.com>
2016-08-02 13:34:17 +0200

    chore: changes server ip

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ touch dist/version.json
 cp -R node_modules dist/node_modules
 echo "{ \"version\": \"$TRAVIS_TAG\" }" > dist/version.json
 tar -czf transfer.tgz dist
-scp -o "StrictHostKeyChecking no" transfer.tgz jenkins@159.203.18.58:/var/www/meditation-plus
-ssh -o "StrictHostKeyChecking no" jenkins@159.203.18.58 'cd /var/www/meditation-plus; cp config/config.json ./; rm -rf app .babelrc config server.conf server.js sockets node_modules version.json; tar -xzf transfer.tgz --strip 1; mv config.json config/config.json; rm transfer.tgz'
+scp -o "StrictHostKeyChecking no" transfer.tgz jenkins@159.203.6.130:/var/www/meditation-plus
+ssh -o "StrictHostKeyChecking no" jenkins@159.203.6.130 'cd /var/www/meditation-plus; cp config/config.json ./; rm -rf app .babelrc config server.conf server.js sockets node_modules version.json; tar -xzf transfer.tgz --strip 1; mv config.json config/config.json; rm transfer.tgz'
