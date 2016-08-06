6d30db16c19239adaee0af09cabc3153747db8f7
Sebastian Kussl <sebastian.kussl@yahoo.de>
2016-08-06 11:07:37 +0200

    feat: update DB scheme

--- src/app/models/testimonials.model.js
+++ src/app/models/testimonials.model.js
@@ -10,9 +10,9 @@ import mongoose from 'mongoose';
 // Define the schema for the showcase item
 let testimonialSchema = mongoose.Schema({
   text : { type: String, required: true },
-  user : { type : mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
-  anonymous: { type : Boolean, required: true },
-  reviewed: { type : Boolean, required: true }
+  user : { type : mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  reviewed: { type : Boolean, required: true },
+  anonymous: { type : Boolean, required: false }
 }, {
     timestamps: true
 });
