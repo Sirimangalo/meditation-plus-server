52687cb92bf1d6a9b619c2330bbd8af3639c3304
Sebastian <sebastian.kussl@yahoo.de>
2016-09-01 01:10:07 +0200

    fix: remove 'notabs' from user model

--- src/app/models/user.model.js
+++ src/app/models/user.model.js
@@ -20,7 +20,6 @@ let userSchema = mongoose.Schema({
   name: { type: String, maxlength: 30 },
   role : { type : String },
   showEmail: Boolean,
-  noTabs: Boolean,
   hideStats: Boolean,
   description: { type: String, maxlength: 300 },
   website: { type: String, maxlength: 100 },
