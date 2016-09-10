e58da2a6f678711a4859ab083a8caa3fa18aecfe
Martin <bieder.martin@googlemail.com>
2016-09-11 00:29:54 +0200

    chore(deploy): adds support for test deployment

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-echo "Deploying $TRAVIS_TAG to server"
-chmod 600 deploy_key
-mv deploy_key ~/.ssh/id_rsa
-touch dist/version.json
-cp -R node_modules dist/node_modules
-echo "{ \"version\": \"$TRAVIS_TAG\" }" > dist/version.json
-tar -czf transfer.tgz dist
-scp -o "StrictHostKeyChecking no" transfer.tgz jenkins@159.203.6.130:/var/www/meditation-plus
-ssh -o "StrictHostKeyChecking no" jenkins@159.203.6.130 'cd /var/www/meditation-plus; cp config/config.json ./; rm -rf app .babelrc config server.conf server.js sockets node_modules version.json; tar -xzf transfer.tgz --strip 1; mv config.json config/config.json; rm transfer.tgz'
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  # Determine folder and version based on tag or branch
+  if [ ! -z "$TRAVIS_TAG" ]; then
+    echo "PROD: Deploying $TRAVIS_TAG to server"
+    folder = "meditation-plus"
+    version = $TRAVIS_TAG
+  else if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "TEST: Deploying master to test server"
+    folder = "meditation-plus-test"
+    version = $TRAVIS_BUILD_NUMBER
+  else
+    exit
+  fi
+
+  chmod 600 deploy_key
+  mv deploy_key ~/.ssh/id_rsa
+  cp -R node_modules dist/node_modules
+
+  # Update version
+  npm --no-git-tag-version $version
+  cp package.json dist/package.json
+
+  tar -czf transfer.tgz dist
+  scp -o "StrictHostKeyChecking no" transfer.tgz jenkins@159.203.6.130:/var/www/$folder
+  ssh -o "StrictHostKeyChecking no" jenkins@159.203.6.130 'cd /var/www/$folder; cp config/config.json ./; rm -rf app .babelrc config server.conf server.js sockets node_modules package.json; tar -xzf transfer.tgz --strip 1; mv config.json config/config.json; rm transfer.tgz'
+fi
