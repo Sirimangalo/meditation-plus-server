6ff349efba666c3b0c694e12a7aed3d47749b88c
binarious <binarious@users.noreply.github.com>
2016-09-11 19:19:45 +0200

    feat(message): adds ability to edit and delete messages (#44)

--- src/app/models/message.model.js
+++ src/app/models/message.model.js
@@ -2,7 +2,9 @@ import mongoose from 'mongoose';
 
 let messageSchema = mongoose.Schema({
   text : { type: String, required: true, maxlength: 1000 },
-  user : { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+  user : { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  edited: Boolean,
+  deleted: Boolean
 }, {
     timestamps: true
 });
--- src/app/routes/_message.router.js
+++ src/app/routes/_message.router.js
@@ -1,6 +1,6 @@
 import Message from '../models/message.model.js';
 import meditatedRecently from './meditatedRecently.js';
-
+let ObjectId = require('mongoose').Types.ObjectId;
 import moment from 'moment';
 
 export default (app, router, io) => {
@@ -135,4 +135,113 @@ export default (app, router, io) => {
     }
   });
 
+  /**
+   * @api {put} /api/message/:id Edit new message
+   * @apiName EditMessage
+   * @apiGroup Message
+   *
+   * @apiParam {String} text Message body
+   */
+  router.put('/api/message/:id', async (req, res) => {
+    try {
+      let message = await Message.findOne({
+        _id: ObjectId(req.params.id)
+      })
+      .exec();
+
+      if (!message) return res.sendStatus(404);
+
+      // only allow to edit own messages (or being admin)
+      if (message.user != req.user._doc._id &&
+        req.user._doc.role !== 'ROLE_ADMIN') {
+        return res.sendStatus(400);
+      }
+
+      // only allow to edit messages for 30 minutes
+      if (req.user._doc.role !== 'ROLE_ADMIN') {
+        const created = moment(message.createdAt);
+        const now = moment();
+        const duration = moment.duration(now.diff(created));
+
+        if (duration.asMinutes() >= 30) {
+          return res.sendStatus(400);
+        }
+      }
+
+      message.text = req.body.text;
+      message.edited = true;
+      await message.save();
+
+      // add user details for response and broadcast
+      let populated = await message.populate(
+        'user',
+        'name gravatarHash country lastMeditation'
+      ).execPopulate();
+
+      let leanObject = populated.toObject();
+      leanObject.ago = moment(leanObject.createdAt).fromNow();
+      if (leanObject.user) {
+        leanObject.user.meditator = meditatedRecently(leanObject.user);
+      }
+
+      // sending broadcast WebSocket message
+      io.sockets.emit('message-update', {
+        message,
+      });
+
+      res.json(leanObject);
+    } catch (err) {
+      res
+        .status(err.name === 'ValidationError' ? 400 : 500)
+        .send(err);
+    }
+  });
+
+  /**
+   * @api {delete} /api/message/:id Delete message
+   * @apiName DeleteMessage
+   * @apiGroup Message
+   */
+  router.delete('/api/message/:id', async (req, res) => {
+    try {
+      let message = await Message.findOne({
+        _id: ObjectId(req.params.id)
+      })
+      .exec();
+
+      if (!message) return res.sendStatus(404);
+
+      // only allow to edit own messages (or being admin)
+      if (message.user != req.user._doc._id &&
+        req.user._doc.role !== 'ROLE_ADMIN') {
+        return res.sendStatus(400);
+      }
+
+      message.deleted = true;
+      await message.save();
+
+      // add user details for response and broadcast
+      let populated = await message.populate(
+        'user',
+        'name gravatarHash country lastMeditation'
+      ).execPopulate();
+
+      let leanObject = populated.toObject();
+      leanObject.ago = moment(leanObject.createdAt).fromNow();
+      if (leanObject.user) {
+        leanObject.user.meditator = meditatedRecently(leanObject.user);
+      }
+
+      // sending broadcast WebSocket message
+      io.sockets.emit('message-update', {
+        message,
+      });
+
+      res.json(leanObject);
+    } catch (err) {
+      res
+        .status(err.name === 'ValidationError' ? 400 : 500)
+        .send(err);
+    }
+  });
 };
