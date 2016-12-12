456cd09905adeca3637cf01fa59bdc4128bc91cc
Sebastian <sebastian.kussl@yahoo.de>
2016-12-12 23:11:12 +0100

    feat: add search & changes names

--- src/app/models/wiki.model.js
+++ src/app/models/wiki.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 let wikiSchema = mongoose.Schema({
   title: { type: String, required: true, unique: true },
-  url: { type: String, required: true, unique: true },
+  videoID: { type: String, required: true, unique: true },
   duration: { type: String },
   category: { type: String, required: true, maxlength: 50 },
   tags: [{ type: String, required: true, unique: true, maxlength: 30 }],
