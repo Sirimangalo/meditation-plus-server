f3fea0337b172708609e69d4641a67b1a132a98e
Sebastian <sebastian.kussl@yahoo.de>
2017-01-04 20:28:58 +0100

    feat: add more routes & tidy code

--- src/app/models/wiki.model.js
+++ src/app/models/wiki.model.js
@@ -1,9 +1,8 @@
 import mongoose from 'mongoose';
 
 let wikiSchema = mongoose.Schema({
-  title: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
   videoID: { type: String, required: true, unique: true },
-  duration: { type: String },
   category: { type: String, required: true, maxlength: 50 },
   tags: [{ type: String, required: true, unique: true, maxlength: 30 }],
 });
