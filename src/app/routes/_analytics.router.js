3f12fdc118e105aa348da2882eae86904d0b8ed7
Sebastian K <sebastian.kussl@yahoo.de>
2017-03-04 13:28:22 +0100

    feat: signup-history route

--- src/app/routes/_analytics.router.js
+++ src/app/routes/_analytics.router.js
@@ -1,12 +1,11 @@
 import User from '../models/user.model.js';
 import Analytics from '../models/analytics.model.js';
 
+let ObjectId = require('mongoose').Types.ObjectId;
+
 export default (app, router, admin) => {
   router.get('/api/analytics-users', admin, async (req, res) => {
-    console.log("§");
     try {
-      // const data = Analytics.findOne();
-
       const count = await User.count();
 
       const admins = await User
@@ -31,14 +30,12 @@ export default (app, router, admin) => {
         .count()
         .exec();
 
-      // // Update
-      // const newUsers = await User
-      //   .find({
-      //     _id: { $gte: data.lastUpdated }
-      //   })
-      //   .exec();
-
-      // Update possible profile changes
+      // Update
+      const newUsers = await User
+        .find({
+          _id: { $gte: data.lastUpdated }
+        })
+        .exec();
 
       res.json({
         count: count,
@@ -53,7 +50,6 @@ export default (app, router, admin) => {
 
   router.get('/api/analytics-countries', admin, async (req, res) => {
     try {
-      console.log("get");
       User.aggregate([
         {
           $group: {
@@ -67,14 +63,12 @@ export default (app, router, admin) => {
         res.json(countries);
       });
     } catch (err) {
-      console.log("err");
       res.status(500).send(err);
     }
   });
 
   router.get('/api/analytics-timezones', admin, async (req, res) => {
     try {
-      console.log("get");
       User.aggregate([
         {
           $group: {
@@ -88,7 +82,47 @@ export default (app, router, admin) => {
         res.json(timezones);
       });
     } catch (err) {
-      console.log("err");
+      res.status(500).send(err);
+    }
+  });
+
+  router.post('/api/analytics-signups', admin, async (req, res) => {
+    try {
+      // Default interval: 1 day (= 864E5 ms)
+      const interval = req.body.interval ? req.body.interval : 864E5;
+      // Default interval: Today - 7 days (= 6048E5 ms)
+      const minDate = req.body.minDate ? req.body.minDate : Date.now() - 6048E5;
+
+      let data = {
+        data: [],
+        labels: []
+      };
+
+      let dtEnd = Date.now();
+      let dtStart = dtEnd - interval;
+
+      console.log(minDate, interval, dtEnd, dtStart);
+      while (dtStart > minDate) {
+
+        const minObjID = ObjectId(Math.floor(dtStart/1000).toString(16) + "0000000000000000");
+        const maxObjID = ObjectId(Math.floor(dtEnd/1000).toString(16) + "0000000000000000");
+
+        const userCount = await User
+          .find({
+            _id: { $gte: minObjID, $lte: maxObjID }
+          })
+          .count();
+
+        data.data.push(userCount);
+        data.labels.push(new Date(dtEnd));
+
+        dtStart -= interval;
+        dtEnd -= interval;
+      }
+
+      res.json(data);
+    } catch (err) {
+      console.log(err);
       res.status(500).send(err);
     }
   });
