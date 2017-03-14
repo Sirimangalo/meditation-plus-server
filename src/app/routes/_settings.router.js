236ca5e793588f6c1f4b0bc13231c761f131c92d
Sebastian Kussl <sebastian.kussl@yahoo.de>
2017-03-14 18:25:04 +0100

    feat: add model & routes for settings

---
+++ src/app/routes/_settings.router.js
@@ -0,0 +1,35 @@
+import settingsHelper from '../helper/settings.js';
+
+export default (app, router, admin) => {
+  /**
+   * @api {post} /api/settings/appointments Update appointment's increment (of hours) parameter
+   * @apiName UpdateAppointmentIncrement
+   * @apiGroup Settings
+   */
+  router.post('/api/settings/appointments', admin, async (req, res) => {
+    try {
+      const increment = req.body.increment ? req.body.increment : 0;
+      await settingsHelper.set('appointmentIncrement', increment);
+
+      res.sendStatus(200);
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  /**
+   * @api {get} /api/settings/appointments Get appointment's increment (of hours) parameter
+   * @apiName GetAppointmentIncrement
+   * @apiGroup Settings
+   */
+  router.get('/api/settings/appointments', admin, async (req, res) => {
+    try {
+      const increment = await settingsHelper.get('appointmentIncrement');
+
+      res.json(increment ? increment : 0);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+  });
+}
