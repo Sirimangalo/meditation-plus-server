90c4216bcb5d1663da4216a9bbcaa07255d7ffb9
Sebastian K <sebastian.kussl@yahoo.de>
2017-06-10 15:25:30 +0200

    fix(push): specify conditions for push messages to pass (#149)

--- src/app/helper/push.js
+++ src/app/helper/push.js
@@ -10,7 +10,7 @@ const push = {
    * @param  username   either username or query to find user(s)
    * @param  data       payload for notification
    */
-  send: (username, data) => {
+  send: (username, data, force = false) => {
     if (!data) {
       return;
     }
@@ -26,16 +26,21 @@ const push = {
           webpush.sendNotification(JSON.parse(sub.subscription), JSON.stringify(data))
         ));
     } else if (typeof username === 'object') {
-      const now = Date.now();
+      const now = new Date();
+      // prevent incorrect date comparison (for performance related differences in milliseconds)
+      // to cause the meditation alarm to not pass.
+      now.setSeconds(now.getSeconds() - 3);
 
       // interpret as query and run push.send() recursively
       User
         .find(username)
         .then(users => users.map(user => {
-          // make notification silent if user is in meditation session
-          data.silent = !(data.meditationAlarm === true || now > user.lastMeditation);
-
-          push.send(user.username, data);
+          // if the user is in a meditation session, only let the alarm through.
+          // But if he's not, then don't let the alarm through (caught a stopped session).
+          // The logical condition is like !(A xor B)
+          if (force === (now <= user.lastMeditation)) {
+            push.send(user.username, data);
+          }
         }));
     }
   }
