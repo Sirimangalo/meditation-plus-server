40f7589a9583e3e6f567a5c6cf49b7e5a844a124
Sebastian <sebastian.kussl@yahoo.de>
2017-06-29 11:18:11 +0200

    feat: add 'interfaces' for required routes

--- src/app/routes/_wiki.router.js
+++ src/app/routes/_wiki.router.js
@@ -2,4 +2,33 @@ import WikiEntry from '../models/wikiEntry.model.js';
 import WikiTag from '../models/wikiTag.model.js';
 
 export default (app, router, admin) => {
+
+  // query
+  router.post('/api/wiki', async (req, res) => {
+    try {
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.post('/api/wiki/new', async (req, res) => {
+    try {
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.post('/api/wiki/modify', async (req, res) => {
+    try {
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
+
+  router.delete('/api/wiki/:id', admin, async (req, res) => {
+    try {
+    } catch (err) {
+      res.status(500).send(err);
+    }
+  });
 };
