12d4be6dd7c20135604697e3ac99fe5bc89111fc
jiixing <jiixing@yahoo.com>
2017-07-17 14:20:33 +0800

    wip

--- src/scripts/populate-dev.js
+++ src/scripts/populate-dev.js
@@ -1,22 +1,47 @@
-let FS = require("q-io/fs");
+const FS = require("q-io/fs");
+import User from '../app/models/user.model'
+import mongooseConf from '../config/mongoose.conf.js';
+import { validateEnvVariables } from '../config/env.conf.js';
+import mongoose from 'mongoose';
 
-function parse_json(content) {
-    const file_content = JSON.parse(content);
-    const example = file_content[0];
-    console.log(example)
+async function saveRow(tableName,data) {
+    let row = new User (data)
+    console.log('saving row')
+    return await row.save()
+//return 'ok'
+}
+async function parse_json(content) {
+    const fileContent = JSON.parse(content);
+    const data = fileContent[0];
+    console.log(data)
 
-    console.log('exit')
+    let saveResult= await saveRow('user',data)
+    console.log('save result',saveResult)
 }
 
-function read_json(filepath) {
-    //can await here
-    FS.read(filepath).then(
-        parse_json
-    ).catch((err)=> {
+async function read_json(filepath) {
+    //move try to main?
+        let content = await FS.read(filepath)
+        await parse_json(content)
+    } catch (err) {
         console.log('error in handler ' + err)
-    })
+    }
+    finally {
+        mongoose.connection.close();
+        console.log('exit')
+    }
+
 }
 
-module.exports = function start() {
-    read_json('./dev-data/user.json')
+function init()
+{
+    validateEnvVariables();
+    mongooseConf(mongoose);
+}
+
+
+module.exports = function start(tables) {
+
+    init()
+    tables.map(t=>read_json(`./dev-data/${t}.json')
 }
