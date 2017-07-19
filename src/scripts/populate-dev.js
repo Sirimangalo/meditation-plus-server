8712567672098a932fd2e481104e8180df60c6b2
jiixing <jiixing@yahoo.com>
2017-07-20 07:28:40 +0800

    rename function and change indentation

--- src/scripts/populate-dev.js
+++ src/scripts/populate-dev.js
@@ -1,117 +1,119 @@
-const FS = require("q-io/fs");
+const FS = require('q-io/fs');
 import mongooseConf from '../config/mongoose.conf.js';
 import {validateEnvVariables} from '../config/env.conf.js';
 import mongoose from 'mongoose';
 import _ from 'lodash';
 
-function getKlass(tableName) {
-    let klass = require(`../app/models/${tableName}.model`)
-    if (!klass)
-        throw new Error(`file ../app/models/${tableName}.model not found`)
-    klass = klass.default // import default equivalent
-    if (!klass)
-        throw new Error(`file definition ../app/models/${tableName}.model not found`)
-    return klass
+/**
+ * convert modelName parameter to a class definition
+ * @param modelName
+ * @returns User.model, Question.model, etc
+ */
+function getModel(modelName) {
+  let model = require(`../app/models/${modelName}.model`);
+  if (!model)
+    throw new Error(`file ../app/models/${modelName}.model not found`);
+  model = model.default; // import default equivalent
+  if (!model)
+    throw new Error(`file definition ../app/models/${modelName}.model not found`);
+  return model;
 }
 
 async function saveRow(klass, data) {
-    // find duplicate
-    let clean_data = _.omit(data, _.keys(mapping))
-    // console.log("clean_data",clean_data)
-    let existing = await klass.findOne(clean_data)
+  // find duplicate
+  let cleanData = _.omit(data, _.keys(mapping));
+  let existing = await klass.findOne(cleanData);
 
-    if (!existing) {
+  if (!existing) {
+    let findResult = {};
+    let mappedData = _.pick(data, _.keys(mapping));
+    let keys = _.keys(mappedData);
+    let findErr = false;
 
-        let find_result = {}
-        let mapped_data = _.pick(data, _.keys(mapping))
-        let keys = _.keys(mapped_data)
-        let find_err = false
+    for (let k of keys) {
+      let res = await mapping[k](data[k]);
 
-        for (let k of keys) {
-            let res = await mapping[k](data[k])
-
-            if (res) {
-                //collect find_result
-                find_result = Object.assign(find_result, res)
-            }
-            else {
-                find_err = true
-                console.log(`mapping error: ${k} not found`, data[k])
-                break
-            }
-        }
-
-        if (!find_err) {
-            // merge custom mapped field back to data
-            let new_data = Object.assign(clean_data, find_result)
-            let row = new klass(new_data)
-            await row.save()
-            console.log("saving row", data)
-        }
+      if (res) {
+        //collect findResult
+        findResult = Object.assign(findResult, res);
+      }
+      else {
+        findErr = true;
+        console.log(`mapping error: ${k} not found`, data[k]);
+        break;
+      }
     }
-    else {
-        console.log("duplicate row", data)
+
+    if (!findErr) {
+      // merge custom mapped field back to data
+      let newData = Object.assign(cleanData, findResult);
+      let row = new klass(newData);
+      await row.save();
+      console.log('saving row', data);
     }
+  } else {
+    console.log('duplicate row', data);
+  }
 }
 async function parse_json(tableName, content) {
-    // json content file has to be an array
-    const fileContent = JSON.parse(content);
-    let klass = getKlass(tableName)
-    for (let row of fileContent) {
-        try {
-            await saveRow(klass, row)
-        } catch (err) {
-            console.log('save exception ' + err.message, row)
-        }
-
+  // json content file has to be an array
+  const fileContent = JSON.parse(content);
+  let model = getModel(tableName);
+  for (let row of fileContent) {
+    try {
+      await saveRow(model, row);
+    } catch (err) {
+      console.log('save exception ' + err.message, row);
     }
+
+  }
 }
 
 async function readJson(t, filepath) {
-    try {
-        console.log('start file ', filepath)
-        let content = await FS.read(filepath)
-        await parse_json(t, content)
-        console.log('file processing finished', filepath, "\n\n")
-    } catch (err) {
-        console.log('readJson error ' + err)
-    }
+  try {
+    console.log('start file ', filepath);
+    let content = await FS.read(filepath);
+    await parse_json(t, content);
+    console.log('file processing finished', filepath, '\n\n');
+  } catch (err) {
+    console.log('readJson error ' + err);
+  }
 }
 
 function init() {
-    validateEnvVariables();
-    mongooseConf(mongoose);
+  validateEnvVariables();
+  mongooseConf(mongoose);
 }
 
 
 /**
  * convert map-user to user.id
  */
-let user_id_functor = async (data) => {
-    let klass = getKlass('user')
-    let user = await klass.findOne(data)
-    if (user)
-        return {user: user._id}
-    return null
-}
+let userMapper = async (data) => {
+  let model = getModel('user');
+  let user = await model.findOne(data);
+  if (user)
+    return {user: user._id};
+  return null;
+};
 
 /**
  * convert map-local to local by hashing local.password
  */
-let password_functor = async (data) => {
-    let ret = data
-    let klass = getKlass('user')
+let userLocalMapper = async (data) => {
+  let ret = data;
+  let model = getModel('user');
 
-    ret.password = new klass().generateHash(ret.password)
-    return {local:ret}
-}
+  ret.password = new model().generateHash(ret.password);
+  return {local: ret};
+};
 /**
  * mapping dictionary that map a field in the dev-data jsons
  */
 const mapping = {
-    "map-user": user_id_functor,
-    "map-local": password_functor
-}
+  'map-user': userMapper,
+  'map-local': userLocalMapper
+};
 
 /**
  * this is es7 code so has to be called by a shell that has babel-register,e.g populate-runner.js
@@ -120,16 +122,11 @@ const mapping = {
  */
 module.exports = async function start(tables) {
 
-    init()
-    //  await test() // when developing new mapping functor
-    for (let t of tables) {
-        await readJson(t, `./dev-data/${t}.json`)
-    }
-    /*
-     let promises = tables.map(t => readJson(t, `./dev-data/${t}.json`))
-     let results = await Promise.all(promises); // magic statement to wait on a loop
-     */
-
-    console.log('after all processing')
-    mongoose.connection.close();
-}
+  init();
+  //  await test() // when developing new mapping functor
+  for (let t of tables) {
+    await readJson(t, `./dev-data/${t}.json`);
+  }
+  console.log('after all processing');
+  mongoose.connection.close();
+};
