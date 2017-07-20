86fe89548126502604e346fe2de1ab3882b32e46
Sebastian <sebastian.kussl@yahoo.de>
2017-07-20 16:55:52 +0200

    feat(settings): add property for devices subscribed to appointments push notifications

--- src/app/models/settings.model.js
+++ src/app/models/settings.model.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 
 let settingsSchema = mongoose.Schema({
-  appointmentsIncrement: { type: Number, default: 0 }
+  appointmentsIncrement: {
+    type: Number,
+    default: 0
+  },
+  appointmentTicker: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'PushSubscriptions',
+    default: []
+  }]
 });
 
 export default mongoose.model('Settings', settingsSchema);
