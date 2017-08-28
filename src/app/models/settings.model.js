14f85ddd9cdd8ab5779dd054b48ab4caa2b45792
Sebastian <sebastian.kussl@yahoo.de>
2017-08-28 18:27:42 +0200

    fix: use moment compatible timezone default in settings

--- src/app/models/settings.model.js
+++ src/app/models/settings.model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 let settingsSchema = mongoose.Schema({
   appointmentsIncrement: { type: Number, default: 0 },
-  appointmentsTimezone: { type: String, default: 'Eastern Standard Time' }
+  appointmentsTimezone: { type: String, default: 'America/Toronto' }
 });
 
 export default mongoose.model('Settings', settingsSchema);
