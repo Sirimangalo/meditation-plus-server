a2787fd6c8ca238a3b7ceb95b2bcd304b528770e
Sebastian <sebastian.kussl@yahoo.de>
2017-09-09 17:06:26 +0200

    fix(appointment-helper): change scope of notify function (#172)

--- src/app/helper/appointment.js
+++ src/app/helper/appointment.js
@@ -4,7 +4,7 @@ import User from '../models/user.model.js';
 import moment from 'moment-timezone';
 import webpush from 'web-push';
 
-const appointmentHelper = {
+let appointmentHelper = {
   /**
    * Parses hour as String.
    *
@@ -39,74 +39,74 @@ const appointmentHelper = {
     }
 
     return appointment;
-  },
-
-  /**
-   * Sends Push Notifications to all subscribed Aamins
-   */
-  notify: async () => {
-    const settings = await Settings.findOne();
-
-    if (!settings || !settings.appointmentsTimezone) {
-      return;
-    }
-
-    // Find admins subscribed to appointments
-    const subscribedUsers = await User
-      .find({
-        role: 'ROLE_ADMIN',
-        'notfications.appointment': { $exists: true, $ne: [] }
-      })
-      .populate('notifications.appointment');
-
-    if (!subscribedUsers) {
-      return;
-    }
-
-    // Find next appointment for today
-    const now = moment.tz(settings.appointmentsTimezone);
-
-    let nextAppointment = await Appointment
-      .findOne({
-        user: { $exists: true, $ne: null },
-        weekDay: now.weekday(),
-        hour: {
-          $gte: now.hours() * 100 + now.minutes(),
-        }
-      })
-      .populate('user', 'name gravatarHash')
-      .then();
-
-
-    if (!nextAppointment) {
-      return;
-    }
-
-    if (settings.appointmentsIncrement) {
-      // add global increment
-      nextAppointment = this.addIncrement(nextAppointment, settings.appointmentsIncrement);
-    }
-
-    // notification object for push message
-    const notification = {
-      title: 'Next Appointment',
-      body: `${nextAppointment.user.name} is scheduled for ${this.printHour(nextAppointment.hour)} ${now.format('z')}`,
-      tag: 'appointment-ticker',
-      icon: nextAppointment.user.gravatarHash.length === 32
-        ? `https://www.gravatar.com/avatar/${nextAppointment.user.gravatarHash}?s=192`
-        : null,
-      silent: true,
-      data: {
-        url: '/schedule',
-        sticky: true
-      },
-      sticky: true // not supported by browsers (yet)
-    };
-
-    subscribedUsers.map(user =>
-      webpush.sendNotification(JSON.parse(user.notifications.appointment.subscription), JSON.stringify(notification))
-    );
   }
+};
+
+/**
+ * Sends Push Notifications to all subscribed Aamins
+ */
+appointmentHelper.notify = async () => {
+  const settings = await Settings.findOne();
+
+  if (!settings || !settings.appointmentsTimezone) {
+    return;
+  }
+
+  // Find admins subscribed to appointments
+  const subscribedUsers = await User
+    .find({
+      role: 'ROLE_ADMIN',
+      'notfications.appointment': { $exists: true, $ne: [] }
+    })
+    .populate('notifications.appointment');
+
+  if (!subscribedUsers) {
+    return;
+  }
+
+  // Find next appointment for today
+  const now = moment.tz(settings.appointmentsTimezone);
+
+  let nextAppointment = await Appointment
+    .findOne({
+      user: { $exists: true, $ne: null },
+      weekDay: now.weekday(),
+      hour: {
+        $gte: now.hours() * 100 + now.minutes(),
+      }
+    })
+    .populate('user', 'name gravatarHash')
+    .then();
+
+
+  if (!nextAppointment) {
+    return;
+  }
+
+  if (settings.appointmentsIncrement) {
+    // add global increment
+    nextAppointment = appointmentHelper.addIncrement(nextAppointment, settings.appointmentsIncrement);
+  }
+
+  // notification object for push message
+  const notification = {
+    title: 'Next Appointment',
+    body: `${nextAppointment.user.name} is scheduled for ${appointmentHelper.printHour(nextAppointment.hour)} ${now.format('z')}`,
+    tag: 'appointment-ticker',
+    icon: nextAppointment.user.gravatarHash.length === 32
+      ? `https://www.gravatar.com/avatar/${nextAppointment.user.gravatarHash}?s=192`
+      : null,
+    silent: true,
+    data: {
+      url: '/schedule',
+      sticky: true
+    },
+    sticky: true // not supported by browsers (yet)
+  };
+
+  subscribedUsers.map(user =>
+    webpush.sendNotification(JSON.parse(user.notifications.appointment.subscription), JSON.stringify(notification))
+  );
 };
 
 export default appointmentHelper;
