2efec989d4c60601be4f574e1f3fc4cb7dd31e5a
Sebastian <sebastian.kussl@yahoo.de>
2017-09-13 18:08:00 +0200

    feat(settings): add property for a information text for broadcasts (#174)

--- src/app/models/settings.model.js
+++ src/app/models/settings.model.js
@@ -8,7 +8,8 @@ let settingsSchema = mongoose.Schema({
   appointmentsTimezone: {
     type: String,
     default: 'America/Toronto'
-  }
+  },
+  livestreamInfo: String
 });
 
 export default mongoose.model('Settings', settingsSchema);
