dddf9ef00f98100cdef8ae9f23a8d53a7733f2c5
Sebastian <sebastian.kussl@yahoo.de>
2017-10-07 10:39:03 +0200

    feat: various improvements in videochat socket

--- src/sockets/appointment.js
+++ src/sockets/appointment.js
@@ -11,14 +11,37 @@ export default (socket, io) => {
   /**
    * method for getting the room length
    */
-  const roomLength = () => io.sockets.adapter.rooms['AppointCall']
-    ? io.sockets.adapter.rooms['AppointCall'].length
+  const roomLength = () => io.sockets.adapter.rooms['Appointment']
+    ? io.sockets.adapter.rooms['Appointment'].length
     : 0;
 
   /**
    * method for checking if the socket has joined the room
    */
-  const inRoom = () => io.sockets.adapter.sids[socket.id]['AppointCall'] ? true : false;
+  const inRoom = () => io.sockets.adapter.sids[socket.id]['Appointment'] ? true : false;
+
+  /**
+   * Server Events
+   * -------------
+   *
+   *  appointment:  Get appointment for now and optionally join room
+   *  ready:        Inform server that user is ready to call
+   *  message:      Broadcast message to room
+   *  toggleMedia:  Notify room about cam/mic status change
+   *  signal:       Broadcast RTC signalling data to room
+   *  leave:        Leave room
+   *
+   *
+   * Client Events
+   * ------------
+   *
+   *  joined:       React with emitting 'ready'
+   *  ready:        Set initiator status and start WebRTC call if possible
+   *  message:      Show message in chat
+   *  toggleMedia:  Indicate change of opponent's cam/mic
+   *  signal:       Use signalling data for establishing RTC connection
+   *  ended:        Show ending screen
+   */
 
 
   /**
@@ -33,19 +56,20 @@ export default (socket, io) => {
     const roomLen = roomLength();
     const userNow = user();
 
-    // if an authorized user is already in the room 'AppointCall', then this means that a
+    // if an authorized user is already in the room 'Appointment', then this means that a
     // reconnect is given (-> second parameter).
     const appointment = await appointHelper.getNow(userNow, roomLen === 1 && !inRoom());
 
+    // send appointment and status to client
     socket.emit('appointment', {
       appointment: appointment,
-      started: appointment ? roomLen > 0 : true
+      started: appointment && roomLen >= 1
     });
 
     // join appointment if requested and possible
     if (join === true && appointment && roomLen < 2 && !inRoom()) {
-      // let socket join the room 'AppointCall' where the exchanging of data and messages happens
-      socket.join('AppointCall');
+      // let socket join the room 'Appointment' where the exchanging of data and messages happens
+      socket.join('Appointment');
       socket.emit('appointment:joined');
 
       // notify other party
@@ -63,20 +87,28 @@ export default (socket, io) => {
             data: {
               url: '/schedule/call'
             }
-            // TODO: call icon
           }
         );
       }
     }
   });
 
-  socket.on('appointment:ready', () => {
+  /**
+   * The 'ready' event is triggered on the client if he gets a 'joined' event.
+   * It also contains a parameter which indicates if the client wants to be an
+   * initiator (fixes a bug caused when both participants have different media
+   * permissions).
+   *
+   * @param  {Boolean} initiator Whether or not client wants to be initiator for RTC
+   */
+  socket.on('appointment:ready', initiator => {
     const ready = inRoom() && roomLength() === 2;
 
-    socket.emit('appointment:ready', { ready: ready, initiator: false });
+    socket.emit('appointment:ready', { ready: ready, initiator: initiator });
 
     if (ready) {
-      socket.broadcast.to('AppointCall').emit('appointment:ready', { ready: ready, initiator: true });
+      // both participants are there, the call can start.
+      socket.broadcast.to('Appointment').emit('appointment:ready', { ready: ready, initiator: !initiator });
     }
   });
 
@@ -93,7 +125,7 @@ export default (socket, io) => {
 
     const userNow = user();
 
-    io.to('AppointCall').emit('appointment:message', {
+    io.to('Appointment').emit('appointment:message', {
       user: {
         _id: userNow._id,
         name: userNow.name,
@@ -109,12 +141,15 @@ export default (socket, io) => {
    * The ':toggledMedia' event is being used when a user
    * disables his microphone/camera
    */
-  socket.on('appointment:toggledMedia', (audio, video) => {
+  socket.on('appointment:toggleMedia', data => {
     if (!inRoom()) {
       return;
     }
 
-    socket.broadcast.to('AppointCall').emit('appointment:toggledMedia', { audio, video });
+    socket.broadcast.to('Appointment').emit('appointment:toggleMedia', {
+      audio: data.audio,
+      video: data.video
+    });
   });
 
   /**
@@ -128,23 +163,18 @@ export default (socket, io) => {
       return;
     }
 
-    socket.broadcast.to('AppointCall').emit('appointment:signal', data);
+    socket.broadcast.to('Appointment').emit('appointment:signal', data);
   });
 
   /**
    * The ':leave' event is being used for ending the appointment.
    */
-  socket.on('appointment:leave', endCall => {
+  socket.on('appointment:leave', gracefully => {
     if (!inRoom()) {
       return;
     }
 
-    if (endCall) {
-      socket.broadcast.to('AppointCall').emit('appointment:ended', true);
-    } else {
-      socket.broadcast.to('AppointCall').emit('appointment:ready', { ready: false, initiator: false });
-    }
-
-    socket.leave('AppointCall');
+    socket.broadcast.to('Appointment').emit('appointment:ended', gracefully === true);
+    socket.leave('Appointment');
   });
 };
