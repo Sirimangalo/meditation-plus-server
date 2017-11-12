38dde4b48873f53f7c7637bfd01847d550c92929
Sebastian <sebastian.kussl@yahoo.de>
2017-11-12 22:17:56 +0100

    chore: resolve lint issues

--- src/app/helper/profile.js
+++ src/app/helper/profile.js
@@ -133,4 +133,4 @@ export default {
 
     return result;
   }
-}
+};
--- src/app/helper/profile.spec.js
+++ src/app/helper/profile.spec.js
@@ -1,8 +1,1 @@
-import profileHelper from './profile.js';
-import { expect } from 'chai';
-import moment from 'moment-timezone';
-import { _ } from 'lodash';
-
-describe('ProfileHelper', () => {
-  // TODO
-});
+// TODO
